66ba09fa02d265eb58d0fe8c49bbf4fb4d875d81
TiagoGrilloLermy <tlermy@gmail.com>
2023-10-17 00:44:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const alternarTema = document.getElementById('alternarTema');
+  const titulo = document.getElementById('titulo');
+  const ligarSom = new Audio('salamisound-1453300-switch-toggle-or-rotary.mp3');
+  const desligarSom = new Audio('salamisound-5290169-switch-turn-one-time-toggle.mp3'); 
+
+  alternarTema.addEventListener('change', function () {
+    if (alternarTema.checked) {
+      document.body.classList.add('modo-escuro');
+      titulo.innerText = 'Tema Escuro';
+      ligarSom.play(); // Reproduz o som de ligar
+    } else {
+      document.body.classList.remove('modo-escuro');
+      titulo.innerText = 'Tema Claro';
+      desligarSom.play(); // Reproduz o som de desligar
+    }
+  });
+});
+
+// Fonte dos audios salamisound.com
